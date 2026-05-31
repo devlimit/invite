9d706bba2e3259168da2607da1a8e97fcfa31467
devlimit <devlimit12@gmail.com>
2026-05-31 13:56:18 +0900

    feat: invite-02 인트로 변경

--- invitaion/src/components/Intro.jsx
+++ invitaion/src/components/Intro.jsx
@@ -1,10 +1,20 @@
+import { useEffect, useRef } from 'react'
+
 function Intro({ fading, onEnd }) {
+  const videoRef = useRef(null)
+
+  useEffect(() => {
+    const video = videoRef.current
+    if (!video) return
+    video.play().catch(() => onEnd())
+  }, [])
+
   return (
     <div className={`intro ${fading ? 'intro--fading' : ''}`}>
       <video
+        ref={videoRef}
         className="intro__video"
         src={`${import.meta.env.BASE_URL}intro.mp4`}
-        autoPlay
         playsInline
         muted
         onEnded={onEnd}
